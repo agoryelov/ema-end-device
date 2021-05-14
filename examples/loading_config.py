6606f81e7a344c5943458f82383638d03ddf4eb4
Clint <55072157+clintonbf@users.noreply.github.com>
2021-05-14 13:12:18 -0700

    Created working package using PyCharm

--- examples/loading_config.py
+++ examples/loading_config.py
@@ -1,8 +1,10 @@
-from ..config_loader import DeviceConfig
+from config.config_loader import DeviceConfig
+
 
 def main():
-  device_config = DeviceConfig('example_config.yaml')
-  print(device_config.get_device_id())
+    device_config = DeviceConfig('example_config.yaml')
+    print(device_config.get_device_id())
+
 
 if __name__ == "__main__":
-  main()
+    main()
