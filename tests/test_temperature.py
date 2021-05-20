ee312ae0f3075157f1e8e06e929f4c5b23cc80de
Clint <55072157+clintonbf@users.noreply.github.com>
2021-05-20 11:28:59 -0700

    Unit tests on absolute zero.
    Fixed import statements

--- tests/test_temperature.py
+++ tests/test_temperature.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
-import temperature
+from sensor_interface.unit_conversion.temperature import celsius_to_kelvin
+from sensor_interface.unit_conversion.temperature import fahrenheit_to_kelvin
+
 
 class Test(TestCase):
     def test_celsius_to_kelvin(self):
@@ -8,9 +10,20 @@ class Test(TestCase):
 
         self.assertEqual(celsius_to_kelvin(celsius_temp), kelvin_temp)
 
+    def test_celsius_absolute_zero(self):
+        temp = -500
+
+        with self.assertRaises(ValueError):
+            celsius_to_kelvin(temp)
+
     def test_fahrenheit_to_kelvin(self):
         fahrenheit_temp = 212
         kelvin_temp = 373.15
 
         self.assertEqual(fahrenheit_to_kelvin(fahrenheit_temp), kelvin_temp)
 
+    def test_fahrenheit_absolute_zero(self):
+        temp = -500
+
+        with self.assertRaises(ValueError):
+            fahrenheit_to_kelvin(temp)
