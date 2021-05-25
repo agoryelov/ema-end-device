7b6f15bc452fa6f6a697e5672e363d839228f402
judaoa <judaozhong@gmail.com>
2021-05-26 05:13:46 +0800

    updated according to problems pointed out by code reivew*2

--- sensors/Dht22.py
+++ sensors/Dht22.py
@@ -5,7 +5,6 @@ import Adafruit_DHT
 # GPIO lib
 import RPi.GPIO as GPIO           # import RPi.GPIO module
 
-
 # SensorInterface(Top Hierarchy)
 # Author : clintonbf
 # https://github.com/clintonbf/sensor_interface/blob/master/sensors/SensorInterface.py
@@ -18,16 +17,11 @@ from utils.unit_conversion.temperature import celsius_to_kelvin
 
 from sensors.SensorDataFormatter import SensorDataFormatter
 
-
-
-
-
 class Dht22(Dht, SensorDataFormatter):
     """
         Credit for connect_to_port and get_raw_data goes to
         Noah MacRitchie (noah21mac@gmail.com) and Andrey Goryelov (andrey.goryelov@gmail.com)
     """
-
     def __init__(self, uid: int, gpio_in: int,  model : str ) -> None:
         super().__init__(uid,gpio_in,model)
         self.__uid = uid
@@ -36,10 +30,7 @@ class Dht22(Dht, SensorDataFormatter):
         # DHT22:
         self.__model = 22
         self.__reading = ()
-
-
-
-
+        
     # Sets the GPIO to be in input mode
     def connect_to_sensor(self):
         DHT_in = self.__gpio_in
@@ -47,29 +38,9 @@ class Dht22(Dht, SensorDataFormatter):
         GPIO.setmode(GPIO.BCM)                 # choose BCM or BOARD
         status = GPIO.setup(DHT_in, GPIO.IN)  # set a port/pin as an input
         return status
-
-    # # take reading:
-    # def take_reading(self) -> int:
-    #     DHT_in = self.__gpio_in
-    #     # TODO error checking?
-    #     GPIO.setmode(GPIO.BCM)                 # choose BCM or BOARD
-    #     Adafruit_DHT.read(self.__model, DHT_in)
-    #         # take reading:
-
-    # def print_data(self) -> int:
-    #     DHT_in = self.__gpio_in
-    #     # TODO error checking?
-    #     GPIO.setmode(GPIO.BCM)                 # choose BCM or BOARD
-    #     temp, humidity = Adafruit_DHT.read(self.__model, DHT_in)
-    #     #  = Adafruit_DHT.read(self.__model, DHT_in)
-    #     print("temperature=", celsius_to_kelvin(temp), "K")
-    #     print("humidity=", humidity, "%")
-
-    # TODO : no raw data or the raw data is dealt with by AdaFruit Library
-    # def get_raw_data(self) -> bytes:
+    
     def get_uid(self):
         return self.__uid
-    
     
     def format_data(self) -> dict :
         readings = {
